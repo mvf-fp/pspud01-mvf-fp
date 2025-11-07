95504c7cf0d6fe58067e9977bb16182f36316739
mvf-fp <m.vera@fpfempa.es>
2025-11-07 13:12:45 +0100

    UD01 Ej4

---
+++ ej04/main.go
@@ -0,0 +1,26 @@
+﻿package main
+
+import (
+    "fmt"
+    "os/exec"
+)
+
+func main() {
+    var cmd *exec.Cmd
+
+    // En Windows usamos "cmd /C dir" con un nombre inexistente para generar error
+    // En Linux/Mac sería: exec.Command("find", "/etc", "-name", "hosts")
+    cmd = exec.Command("cmd", "/C", "dir", "C:\\Windows\\System32\\drivers\\etc\\hosts")
+
+    // Capturamos stdout + stderr combinados
+    salida, err := cmd.CombinedOutput()
+
+    fmt.Println("Salida combinada del comando:\n")
+    fmt.Println(string(salida))
+
+    if err != nil {
+        fmt.Println("Error de ejecución:", err)
+    } else {
+        fmt.Println("Comando ejecutado correctamente ")
+    }
+}
